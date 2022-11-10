ad05be866a6bab034ce20f2b1e79a46ccfe5b454
Alexey Efesov <rusure72@gmail.com>
2022-11-11 00:14:47 +0500

    Главная страница и Сообщества

---
+++ yatube/posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Сообщества {slug}')
+
+# Create your views here.
