f1a9921122917f1e6b8c423f2ddaebff0d8aa312
Alexey Efesov <rusure72@gmail.com>
2022-11-12 13:35:35 +0500

    html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -52,10 +52,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+# anfisa/settings.py
+
+# Путь к директории с шаблонами вынесен в переменную:
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Добавлено: Искать шаблоны на уровне проекта
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,9 +68,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+            ]
         },
-    },
+    }
 ]
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
