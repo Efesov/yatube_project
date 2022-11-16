bf49e85d6ebe052955a3a77121bdc03dcfb4cebd
Alexey Efesov <rusure72@gmail.com>
2022-11-16 15:22:26 +0500

    opapa

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.template import loader
 
 def index(request):
     template = 'posts/index.html'
     return render(request, template)
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'Сообщества {slug}')
+def group_posts(request, pk):
+    template = 'posts/group_list.html'
+    return render(request, template)
+    
 
 # Create your views here.
